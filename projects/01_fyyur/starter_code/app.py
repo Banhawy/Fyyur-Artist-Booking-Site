3abca23f5283b45e5354c929ceba200cda51a6ab
the.benhawy <the.benhawy@gmail.com>
2020-10-16 05:40:29 -0400

    added genres tables & models

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -45,6 +45,7 @@ class Venue(db.Model):
     seeking_talent = db.Column(db.Boolean,  nullable=False, default=False)
     seeking_description = db.Column(db.String(500))
     shows = db.relationship('Show', backref='venue', lazy=True)
+    genres = db.relationship('Genre', backref='venue', lazy=True)
     def __repr__(self):
       return f'<Venue {self.id} {self.name}>'
 
@@ -64,16 +65,41 @@ class Artist(db.Model):
     seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
     seeking_description = db.Column(db.String(500))
     shows = db.relationship('Show', backref='artist', lazy=True)
+    genres = db.relationship('Genre', backref='artist', lazy=True)
     def __repr__(self):
       return f'<Artist {self.id} {self.name}>'
     
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+# Done Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
   __tablename__ = 'show'
   
   artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), primary_key=True)
   venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), primary_key=True)
   start_time = db.Column(db.DateTime, nullable=False)
+
+class Genre(db.Model):
+  __tablename__ = 'genre'
+  
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  venues = db.relationship('Venue', backref='genre', lazy=True)
+  artists = db.relationship('Artist', backref='genre', lazy=True)
+  
+  def __repr__(self):
+      return f'<Genre {self.id} {self.name}>'
+  
+class Venue_Genre(db.Model):
+  __tablename__ = 'venue_genre'
+  
+  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), primary_key=True)
+  genre = db.Column(db.Integer, db.ForeignKey('genre.id'), primary_key=True)
+  
+class Artist_Genre(db.Model):
+  __tablename__ = 'artist_genre'
+  
+  venue_id = db.Column(db.Integer, db.ForeignKey('artist.id'), primary_key=True)
+  genre = db.Column(db.Integer, db.ForeignKey('genre.id'), primary_key=True)
+  
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
